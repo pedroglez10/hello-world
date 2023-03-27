fbae220489e9a25680c3b521b881885af12daac7
pedroglez10 <garciaglez@outlook.com>
2023-03-27 16:48:43 -0600

    function to capture only numbers

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('entro controlador')
+console.log('entro controlador');
+
+function onlynumbers() {
+  var charCode = (evt.which) ? evt.which : evt.keyCode
+  if (charCode > 31 && (charCode < 48 || charCode > 57))
+    return false;
+  return true;
+}
